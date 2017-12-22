6110102439308e598265a3d59ff58b2d3d1f313b
Slade Utley <SladeUtley@Slades-MacBook-Pro.local>
2017-12-22 12:36:41 -0600

    added dom output mirrors input field event listener, and add color button is working

--- events.js
+++ events.js
@@ -14,16 +14,16 @@ for (let i=0; i<sections.length; i++){
 }
 // When the mouse is over the h1 tag, the output element should contain the text "You moved your mouse over the header".
 let h1tag = document.getElementById("page-title");
-h1tag.addEventListener("mouseover" ,function(){
-    outputTarget.innerHTML = `You moved your mouse over the header`;
-});
+h1tag.addEventListener("mouseover" , () => outputTarget.innerHTML = `You moved your mouse over the header`);
 // When the mouse leaves the h1 tag, the output element should contain the text "You left me!!".
 h1tag.addEventListener("mouseout", function(){
     outputTarget.innerHTML = `You left me!!`
 });
 // When you type characters into the input field, the output element should mirror the text in the input field.
-
+let inputField = document.getElementById("keypress-input");
+inputField.addEventListener("input", () => outputTarget.innerHTML = inputField.value);
 // When you click the "Add color" button, the guinea-pig element's text color should change to blue.
+document.getElementById("add-color").addEventListener("click", () => document.getElementById("guinea-pig").style.color="blue");
 // When you click the "Hulkify" button, the guinea-pig element's font size should become much larger.
 // When you click the "Capture it" button, the guinea-pig element should have a border added to it.
 // When you click the "Rounded" button, the guinea-pig element's border should become rounded.
